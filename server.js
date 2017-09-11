bc40ba58ded90b57346822ffa86cc55a9312f681
gokult7 <31036116+gokult7@users.noreply.github.com>
2017-09-11 19:09:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'https://en.wikipedia.org/wiki/File:Chennai_Super_Kings_Logo.svg));
 });
 
 
