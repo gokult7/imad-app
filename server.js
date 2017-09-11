1b6cd940625836e98a733529348ed2bfbf0b4855
gokult7 <31036116+gokult7@users.noreply.github.com>
2017-09-11 19:09:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
